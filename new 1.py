40cb7a149797860a0750cb57978577a9906b02c1
Abdirahman Farrahand <abdirahmanf@hq.trayport.com>
2017-08-03 14:04:33 +0100

    new stuff added friend insults

---
+++ new 1.py
@@ -0,0 +1,9 @@
+#fill numbers in place
+a = 
+b = 
+c = 
+d = 
+sum = a + b + c + d 
+
+# Display the sum
+print(sum)
---
+++ realfriends.py
@@ -0,0 +1,11 @@
+def realfriends(name):
+  if name == ("zahir uddin"):
+    print("fuck you elephant elbows")
+  elif name == "tamim":
+    print("are you alive?")
+  elif name == ("tanvir"):
+        print("eyetalian")
+  elif name == "musa":
+    print("muuusas thinks hes cool because hes an oxfordboy")
+  else:
+    print("no")
