d478f473a12a90982c47453a688501168ee08a0f
carlton-banks <30654198+carlton-banks@users.noreply.github.com>
2017-08-03 15:25:51 +0100

    Update realfriends.py
    added zahir islam

--- realfriends.py
+++ realfriends.py
@@ -1,9 +1,11 @@
 def realfriends(name):
-  if name == ("zahir uddin"):
+  if name == "zahir uddin":
     print("fuck you elephant elbows")
+  elif name == "zahir islam":
+    print("I have curtains for shoes")
   elif name == "tamim":
     print("are you alive?")
-  elif name == ("tanvir"):
+  elif name == "tanvir":
         print("eyetalian")
   elif name == "musa":
     print("muuusas thinks hes cool because hes an oxfordboy")
